339a68edd82a2157a45b4d4214893d8e757c14de
kenkeluke <halimahogunbiyi@gmail.com>
2022-08-21 00:20:29 +0100

    Manipulating text content

--- app.js
+++ app.js
@@ -2,3 +2,14 @@ const firstCard = document.querySelector('#first');
 
 const secondCard = document.querySelector('#Thank-you');
 
+const selected = document.querySelector('#list');
+
+const button = document.querySelector('button');
+
+selected.addEventListener('click', function(e){
+const value = e.target.textContent;
+
+console.log(value);
+secondCard.childNodes[3].textContent = `You selected ${value} out of 5`;
+})
+
