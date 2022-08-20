df7143940d3f83855dfc3603daa93d0ac1d194b1
kenkeluke <halimahogunbiyi@gmail.com>
2022-08-21 00:57:50 +0100

    Adding comments to app.js

--- app.js
+++ app.js
@@ -8,28 +8,40 @@ const button = document.querySelector('button');
 
 selected.addEventListener('click', function(e)
 {
+//selecting all the list elements aka rating scores
+
     const listItems = selected.querySelectorAll('li');
+
+
+/* a forEach method to remove any previously added active state, in case of multiple clicks */
 
     listItems.forEach((item) => {
     item.classList.remove('active')
     })
 
-	console.log(listItems);
-
+	
+// extracting the value of the clicked score
 	const value = e.target.textContent;
+
+// Getting the actual list element that was clicked out of the five
 	const li = e.target.parentNode;
-		console.log(li);
 
-	li.classList.add('active')
+//Appending the active class to the exact list item
+ li.classList.add('active')
 
 
-	console.log(value);
+/*Value extracted earlier is then used to modify the content of the 
+ second card that displays the score that was chosen*/
 	secondCard.childNodes[3].textContent = `You selected ${value} out of 5`;
 })
 
 
 button.addEventListener('click', function(e){
+/*preventing the default action of a submit button which is to refresh and reset the page */
     e.preventDefault();
+
+//Adding the hidden class to the fist card to hide it after submission
     firstCard.classList.add('hidden');
+//Removing the hidden class on the second card to display it after submission
     secondCard.classList.remove('hidden');
 })
