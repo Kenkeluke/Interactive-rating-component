4151cf0f74aee57141b98b55021362ef7d794125
kenkeluke <halimahogunbiyi@gmail.com>
2022-08-21 07:33:53 +0530

    adding active classes

--- app.js
+++ app.js
@@ -6,8 +6,22 @@ const selected = document.querySelector('#list');
 
 const button = document.querySelector('button');
 
-selected.addEventListener('click', function(e){
+selected.addEventListener('click', function(e)
+{
+const listItems = selected.querySelectorAll('li');
+
+listItems.forEach((item) => {
+    item.classList.remove('active')
+})
+
+console.log(listItems);
+
 const value = e.target.textContent;
+const li = e.target.parentNode;
+console.log(li);
+
+li.classList.add('active')
+
 
 console.log(value);
 secondCard.childNodes[3].textContent = `You selected ${value} out of 5`;
